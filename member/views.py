ef6d447b0ee56bfb86d664524f7970ab312abb34
Chinseok Lee <allieuslee@gmail.com>
2015-05-14 02:34:32 +0900

    0425 - 실습 2: 로그인 / 아웃

--- member/views.py
+++ member/views.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
 from django.template import Context, loader
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.core.context_processors import csrf
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, authenticate
+from django.contrib.auth import login as django_login
+
 from member.models import Profile
 
 # Create your views here.
@@ -28,3 +30,32 @@ def register(request):
 	
 	return HttpResponse(tpl.render(ctx))
 
+def is_login(request):
+	if (request.user.is_authenticated()):
+		return HttpResponse("after login")
+	else:
+		return HttpResponse("before login")
+
+def login(request):
+	tpl = loader.get_template("member/login.html")
+	ctx = Context({
+	})
+	
+	if (request.POST.has_key('username')):
+		username = request.POST['username']
+		password = request.POST['password']
+		user = authenticate(username=username, password=password)
+		
+		if (user != None):
+			# success
+			django_login(request, user)
+			return HttpResponseRedirect("/member/is_login")
+		else:
+			# fail
+			pass
+	
+	ctx.update(csrf(request))
+	
+	return HttpResponse(tpl.render(ctx))
+	
+	
--- pysta/urls.py
+++ pysta/urls.py
@@ -8,5 +8,13 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
 	url(r'^member/register', 'member.views.register'),
-    url(r'^admin/', include(admin.site.urls)),
+
+	url(r'^member/is_login', 'member.views.is_login'),
+
+	url(r'^member/login', 'member.views.login'),
+	url(r'^member/logout$', 'django.contrib.auth.views.logout',{'next_page': '/member/is_login'}, name="logout_url"),
+
+	url(r'^admin/', include(admin.site.urls)),
+	
+	
 )
