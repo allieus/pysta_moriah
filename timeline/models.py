790e4ae25be186368195421807d249209bc0d09d
Chinseok Lee <allieuslee@gmail.com>
2015-05-14 02:40:23 +0900

    0425 - fix

--- timeline/models.py
+++ timeline/models.py
@@ -6,5 +6,5 @@ from django.conf import settings
 class Post(models.Model):
 	contents = models.TextField(null=False)
 	image = models.TextField()
-	owner = models.OneToOneField(settings.AUTH_USER_MODEL)
-	created_at = models.DateTimeField(auto_now_add=True, auto_now=False)
+	owner = models.ForeignKey(settings.AUTH_USER_MODEL)
+	created_at = models.DateTimeField(auto_now_add=True)
--- utils/utils.py
+++ utils/utils.py
@@ -4,25 +4,38 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.template import Context, loader
 import os
-from django.utils.encoding import smart_bytes
+from django.utils.text import slugify
+from urllib import quote
 
-def safe_filename(filename):
-	fn_bytes = smart_bytes(filename)
-	fn_bytes = fn_bytes.replace("\\x", "")
-	fn_bytes = fn_bytes.replace(" ", "_")
+def safe_filename(filename_org):
 	
-	pos = fn_bytes.rfind(".")
-	fn_base = fn_bytes[:pos]
-	fn_ext = fn_bytes[pos:]
-
-	if (len(fn_bytes) > 120):
-		fn_base = fn_base[:115]
+	# 경로, 파일명, 확장자 분리
+	filename = os.path.basename(filename_org)
+	file_dir = os.path.dirname(filename_org)
+	
+	pos = filename.rfind(".")
+	file_base = filename[:pos]
+	file_ext = filename[pos:]
+	
+	# 파일로 허용 안되는 문자 제거
+	file_base = slugify(file_base)
+	
+	# 파일 크기
+	len_tot = len(file_base) + len(file_ext)
+	
+	if (len_tot > 120):	# 120 자 보다 크면,
+		# 잘라낼 크기
+		len_overflow = len_tot - 120
+		file_base = file_base[:-len_overflow]
 	
 	postfix = 0
 	
-	while (os.path.isfile(fn_bytes)):
-		postfix = postfix + 1
-		fn_bytes = "{0}_{1}{2}".format(fn_base,postfix,fn_ext)
+	file_base = os.path.join(file_dir, file_base)
+	filename = u"{0}_{1}{2}".format(file_base, postfix, file_ext)
 	
-	return fn_bytes
+	while (os.path.isfile(filename)):
+		postfix = postfix + 1
+		filename = u"{0}_{1}{2}".format(file_base, postfix, file_ext)
+	
+	return filename
 	
